578b2113ca278c9dfc90f58667d6c6a087afa1d2
Wren Purcell <purcell856@gmail.com>
2022-04-14 19:24:45 -0400

    test api call to backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [booksData, setBooksData] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/books')
+    .then(res => setBooksData(res.data))
+  },[])
+  console.log(booksData)
   return (
     <div className="App">
 
@@ -9,3 +17,4 @@ function App() {
 }
 
 export default App;
+
