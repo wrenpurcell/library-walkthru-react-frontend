b39c0f9996df8de7c85f0f3037b162c8ffe3a80b
Wren Purcell <purcell856@gmail.com>
2022-04-15 14:36:57 -0400

    errors

--- src/components/OneBook.jsx
+++ src/components/OneBook.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { getOneBook } from '../services/api-calls'
 
 export default function OneBook() {
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
     const { id } = useParams()
     const [data, setData] = useState({})
     const [genres, setGenres] = useState([])
@@ -13,14 +13,14 @@ export default function OneBook() {
         .then(res => {
             setData(res.data) 
             setGenres(res.data.genres)})
-    },[])
+    })
 console.log(data)
 
 
     return (
         <div>
             <h1>{data.title}</h1>
-            <img src={data.imageLink} />
+            <img src={data.imageLink} alt={`${data.title} cover art`} />
             {genres.map((genre)=>{
                return <li>{genre}</li>
             })}
